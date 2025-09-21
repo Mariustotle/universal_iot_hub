99615573c0c146e9d8ca7ac1e323413cceb2e921
Marius Vorster <marius.vrstr@gmail.com>
2025-09-21 07:53:15 +0200

    Added minimal test

---
+++ min.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+RELAY_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT, initial=GPIO.HIGH)  # OFF
+
+print("Relay should be OFF (red LED off).")
+time.sleep(3)
+
+GPIO.output(RELAY_PIN, GPIO.LOW)  # ON
+print("Relay ON (red LED should be ON).")
+time.sleep(3)
+
+GPIO.output(RELAY_PIN, GPIO.HIGH)  # OFF
+print("Relay OFF (red LED should be OFF).")
+time.sleep(3)
+
+GPIO.cleanup()
