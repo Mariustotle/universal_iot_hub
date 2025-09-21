24b7d6c9354e1f071d0ac6cb591af65df4240efc
Marius Vorster <marius.vrstr@gmail.com>
2025-09-21 12:40:54 +0200

    Swapped Relay Commands

--- min.py
+++ min.py
@@ -4,16 +4,16 @@ import time
 RELAY_PIN = 18
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RELAY_PIN, GPIO.OUT, initial=GPIO.HIGH)  # OFF
+GPIO.setup(RELAY_PIN, GPIO.OUT, initial=GPIO.LOW)  # OFF
 
 print("Relay should be OFF (red LED off).")
 time.sleep(3)
 
-GPIO.output(RELAY_PIN, GPIO.LOW)  # ON
+GPIO.output(RELAY_PIN, GPIO.HIGH)  # ON
 print("Relay ON (red LED should be ON).")
 time.sleep(3)
 
-GPIO.output(RELAY_PIN, GPIO.HIGH)  # OFF
+GPIO.output(RELAY_PIN, GPIO.LOW)  # OFF
 print("Relay OFF (red LED should be OFF).")
 time.sleep(3)
 
