a5cd3a7e78f29ac47fd71128106c9073f55059ee
Marius Vorster <marius.vrstr@gmail.com>
2025-11-09 15:48:10 +0200

    More granular sim config

--- src/config/app_config.py
+++ src/config/app_config.py
@@ -4,6 +4,7 @@ from typing import Optional
 from common.contracts.logging import Logging
 from peripherals.contracts.adapter_type import AdapterType
 from peripherals.contracts.device_type import DeviceType
+from peripherals.contracts.simulator.simulator_config import SimulatorConfig
 from src.contracts.actuators_config import ActuatorsConfig
 from src.contracts.communications_config import CommunicationsConfig
 from src.contracts.sensors_config import SensorsConfig
@@ -11,7 +12,7 @@ from src.contracts.sensors_config import SensorsConfig
 import json
 
 class AppConfig(BaseModel):
-    Simulator: bool = None
+    Simulator:SimulatorConfig = None
     Device: DeviceType = None
     Adapter: AdapterType = None
     logging: Logging = None
